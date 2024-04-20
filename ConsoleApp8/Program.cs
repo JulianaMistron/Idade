4259c27ad0215d42c8767499ef0412fe91f56e2f
JulianaMistron <jumistron@yahoo.com.br>
2024-04-20 13:36:51 -0300

    Idade

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int maioresIdade = 0;
+int menoresIdade = 0;
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Digite a idade da pessoa " + i + ":");
+    int idade = int.Parse(Console.ReadLine());
+
+
+    if (idade >= 18)
+    {
+        maioresIdade++;
+    }
+    else
+    {
+        menoresIdade++;
+    }
+}
+
+Console.WriteLine("Quantidade de pessoas maiores de idade: " + maioresIdade);
+Console.WriteLine("Quantidade de pessoas menores de idade: " + menoresIdade);
